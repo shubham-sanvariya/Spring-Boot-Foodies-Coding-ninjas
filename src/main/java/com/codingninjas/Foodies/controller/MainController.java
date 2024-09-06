deb3bf2a028c94125f1ae55dc823781660329e3b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-06 15:29:03 +0530

    adding getAllCustomers in controller class

--- src/main/java/com/codingninjas/Foodies/controller/MainController.java
+++ src/main/java/com/codingninjas/Foodies/controller/MainController.java
@@ -1,8 +1,26 @@
 package com.codingninjas.Foodies.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.codingninjas.Foodies.entity.Customer;
+import com.codingninjas.Foodies.entity.Rating;
+import com.codingninjas.Foodies.entity.Restaurant;
+import com.codingninjas.Foodies.service.MainService;
 
 @RestController
 public class MainController {
-    
+    @Autowired
+    MainService mainService;
+
+    @GetMapping("/customers")
+    public List<Customer> getAllCustomers(){
+        return mainService.getAllCustomers();
+    }
 }
--- src/main/java/com/codingninjas/Foodies/service/MainService.java
+++ src/main/java/com/codingninjas/Foodies/service/MainService.java
@@ -5,9 +5,6 @@ import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import com.codingninjas.Foodies.entity.Customer;
 import com.codingninjas.Foodies.entity.Rating;
 import com.codingninjas.Foodies.entity.Restaurant;
