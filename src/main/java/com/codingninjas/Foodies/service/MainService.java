5f8aa06cd6c408d1ff35a2a5da74edc01fa94d98
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-06 14:14:58 +0530

    adding addRatingForRestaurantByCustomer method in service class

--- src/main/java/com/codingninjas/Foodies/service/MainService.java
+++ src/main/java/com/codingninjas/Foodies/service/MainService.java
@@ -1,9 +1,12 @@
 package com.codingninjas.Foodies.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.codingninjas.Foodies.entity.Customer;
 import com.codingninjas.Foodies.entity.Rating;
@@ -38,5 +41,25 @@ public class MainService {
 
     public void addRestaurant(Restaurant restaurant){
         restaurantRepository.save(restaurant);
+    }
+
+    public void  addRatingForRestaurantByCustomer(Rating rating, Integer customerId, String restaurantName){
+        Restaurant restaurant = restaurantRepository.findByName(restaurantName);
+        if (restaurant == null) {
+            throw new NoSuchElementException("no such restaurant by restaurant name: " + restaurantName);
+        }
+
+        Customer customer = customerRepository.findById(customerId)
+        .orElseThrow(() -> new NoSuchElementException("customer not found by customer id: " + customerId));
+
+        rating.setRestaurant(restaurant);
+        rating.setCustomer(customer);
+        ratingRepository.save(rating);
+
+        restaurant.getRatings().add(rating);
+        customer.getRatings().add(rating);
+
+        restaurantRepository.save(restaurant);
+        customerRepository.save(customer);
     }
 }
