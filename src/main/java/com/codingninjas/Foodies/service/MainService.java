2192ea4ae1dfc662c4b74bd504a6cbbd0fc2058a
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-06 13:34:01 +0530

    adding get all ratings method in service class

--- src/main/java/com/codingninjas/Foodies/service/MainService.java
+++ src/main/java/com/codingninjas/Foodies/service/MainService.java
@@ -1,8 +1,28 @@
 package com.codingninjas.Foodies.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.codingninjas.Foodies.entity.Rating;
+import com.codingninjas.Foodies.repository.CustomerRepository;
+import com.codingninjas.Foodies.repository.RatingRepository;
+import com.codingninjas.Foodies.repository.RestaurantRepository;
 
 @Service
 public class MainService {
     
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    @Autowired
+    RatingRepository ratingRepository;
+
+    public List<Rating> getAllRatings(){
+        return ratingRepository.findAll();
+    }
 }
